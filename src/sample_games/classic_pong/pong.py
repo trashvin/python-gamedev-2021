3c89575ba2fffb61349cd57db3a9d53fa69ac2a4
trashvin <m3lles@gmail.com>
2021-05-29 22:17:49 +0800

    update

--- src/sample_games/classic_pong/pong.py
+++ src/sample_games/classic_pong/pong.py
@@ -7,11 +7,31 @@ pygame.init()
 screen = pygame.display.set_mode((400, 600))
 square = pygame.Surface((20, 20))
 square.fill((255, 0, 0))
+
+x =100
+y=100
+exit = 0
 while True:
-    screen.blit(square, (100, 100))
-    if pygame.event.get(pygame.QUIT):
+
+    screen.blit(square, (x,y))
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x -= 10
+            if event.key == pygame.K_RIGHT:
+                x += 10
+            if event.key == pygame.K_UP:
+                y -= 10
+            if event.key == pygame.K_DOWN:
+                y += 10
+        if event.type == pygame.QUIT:
+            exit = 1
+
+    if exit == 0:
+        pygame.display.update()
+    else:
         break
-    pygame.display.update()
  
 pygame.quit()
 
