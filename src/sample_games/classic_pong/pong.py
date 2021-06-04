d5dd34f5f84c69515319ea10710fb0ae818ef8f8
trashvin <m3lles@gmail.com>
2021-06-05 01:12:08 +0800

    update

--- src/sample_games/classic_pong/pong.py
+++ src/sample_games/classic_pong/pong.py
@@ -2,26 +2,36 @@
 
 import pygame
 
+# initialize pygame
 pygame.init()
 
+# define colors
 RED = (255,0,0)
 BLACK = (0,0,0)
 WHITE = (255,255,255)
+
+# create the screen
 screen = pygame.display.set_mode((800, 600))
 
+# paddle 1
 paddle_1 = pygame.Surface((20, 100))
 paddle_1.fill(RED)
 paddle_1_rect = paddle_1.get_rect()
 
-mid = screen.get_size()[1]/2 - (paddle_1.get_size()[1]/2)
+paddle_1_start_x = paddle_1.get_size()[0]
+paddle_1_start_y = screen.get_size()[1]/2 - paddle_1.get_size()[1]/2
+paddle_1_rect.update((paddle_1_start_x, paddle_1_start_y), paddle_1.get_size())
 
-paddle_1_rect = paddle_1_rect.move(paddle_1.get_size()[0],mid)
-
+# paddle 2
 paddle_2 = pygame.Surface((20, 100))
 paddle_2.fill(RED)
 paddle_2_rect = paddle_2.get_rect()
-paddle_2_rect = paddle_2_rect.move(800 - paddle_2.get_size()[0]*2,mid)
 
+paddle_2_start_x = screen.get_size()[0] - (2*paddle_2.get_size()[0])
+paddle_2_start_y = screen.get_size()[1]/2 - paddle_2.get_size()[1]/2
+paddle_2_rect.update((paddle_2_start_x, paddle_2_start_y), paddle_2.get_size())
+
+# ball
 ball_radius = 10
 ball_y = screen.get_size()[1]/2
 ball_x = 30 + paddle_1.get_size()[0] + ball_radius*2
@@ -29,17 +39,24 @@ ball = pygame.Surface((20, 20))
 pygame.draw.circle(ball, WHITE,(ball.get_size()[0]/2,ball.get_size()[1]/2 ), ball_radius)
 ball_rect = ball.get_rect()
 ball_rect.top =screen.get_size()[1]/2
-ball_rect.left = 30 + paddle_1.get_size()[0] + ball_radius*2
+ball_rect.left = 2 * paddle_1.get_size()[0] + ball_radius*2
 
+# center separator
+mid = screen.get_size()[1]/2 - (paddle_1.get_size()[1]/2)
 mid_line = pygame.Surface((2, screen.get_size()[1]))
 mid_line.fill(RED)
 
 direction_1 = 1
 direction_2 = 1
-friction = 2
-
+friction = 4  # the higher the faster
 fps_clock = pygame.time.Clock()
-print(screen.get_size()[0],screen.get_size()[1])
+
+# title
+title_font = pygame.font.SysFont("calibri", 20)
+title = title_font.render("*** PONG ***",1, WHITE)
+
+score_1 =0
+score_2 = 0
 exit = 0
 while True:
     fps_clock.tick(100)
@@ -66,34 +83,39 @@ while True:
 
     # bouncing ball
     if ball_rect.right > screen.get_size()[0]:
+        score_1 +=1
         direction_1 = -1
     if ball_rect.left < 0 :
+        score_2 +=1
         direction_1 = 1
 
     if ball_rect.bottom > screen.get_size()[1]:
         direction_2 = -1
     if ball_rect.top <0:
+        
         direction_2 = 1
-
-    
 
     if ball_rect.colliderect(paddle_1_rect):
         direction_1 = 1;
     if ball_rect.colliderect(paddle_2_rect):
         direction_1 = -1;
-    
-    
+    #print(score_1, score_2)
     dir_x = 1 * direction_1 * friction
     dir_y = 1 * direction_2 * friction
     ball_rect = ball_rect.move((dir_x, dir_y))
-    # print(ball_rect.top,ball_rect.bottom, dir_x, dir_y, )
-    # print(paddle_1.get_rect())
+
+    score_a = title_font.render(str(score_1),1, WHITE)
+    score_b = title_font.render(str(score_2),1, WHITE)
+   
     if exit == 0:
         screen.fill((0,0,0))
         screen.blit(paddle_1, paddle_1_rect)
         screen.blit(paddle_2, paddle_2_rect)  
         screen.blit(ball, ball_rect)
         screen.blit(mid_line,(screen.get_size()[0]/2,0))
+        screen.blit(title, (5,5))
+        screen.blit(score_a, (5,screen.get_size()[1] - 25))
+        screen.blit(score_b, (screen.get_size()[0]-40,screen.get_size()[1] - 25))
         pygame.display.flip()
     else:
         break
