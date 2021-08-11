a860c42534e6dfb60f01b908fb12118cdc78468a
trashvin <m3lles@gmail.com>
2021-08-11 09:55:15 +0800

    changes

--- codes/sample_games/guessing_game/guess.py
+++ codes/sample_games/guessing_game/guess.py
@@ -1,4 +1,34 @@
 # simple guessing game
-# uses if,loop,list and random generator
+# uses if,loop,list and random general
 
-print("Guessing Game")
+import random
+
+number = random.randint(0,100)
+
+correct_guess = False
+
+while(correct_guess == False):
+    try:
+        guess = int(input("Enter your guess number : "))
+
+        if guess == number:
+            print("you are correct")
+            print(f"the generated number is {number}")
+
+            correct_guess = True
+        elif guess < number:
+            print("the number is higher")
+        else:
+            print("the number is lower")
+
+    except:
+        print("please enter a valid number")
+
+
+    
+
+
+
+
+
+
